2cfbb74809b94f1fe328d8f971a5e2749d607ee0
Ivan Osuna <ivan.osuna.ayuste@gmail.com>
2023-09-29 13:53:05 +0200

    angular

--- ejemplos/proyecto/src/app/app-routing.module.ts
+++ ejemplos/proyecto/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './components/app/app.component';
+import { UserlistComponent } from './components/userlist/userlist.component';
+import { ComponenteA } from './components/componente-a/a.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'listado', component: UserlistComponent },
+  // Lazy load de un componente.
+  // Ese componente no se cargará hasta que no lo necesite
+  { path: 'componenteA', loadChildren: () => import('../app/app.module').then(m => m.AppModule) },
+];
+
+//   administracion
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
